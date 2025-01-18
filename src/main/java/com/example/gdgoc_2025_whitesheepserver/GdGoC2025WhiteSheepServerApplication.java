aef6c7bbdccbd8adc202c702525fa65bde3ab782
Lazernes <dbsauddn1@gmail.com>
2025-01-18 17:07:47 +0900

    initial Setting

---
+++ src/main/java/com/example/gdgoc_2025_whitesheepserver/GdGoC2025WhiteSheepServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.gdgoc_2025_whitesheepserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GdGoC2025WhiteSheepServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GdGoC2025WhiteSheepServerApplication.class, args);
+    }
+
+}
